d37f4b34569f5006615dc52e0e26ff9d3824cbed
CRBroughton <crbroughton@posteo.uk>
2023-12-31 10:41:37 +0000

    feat: :construction: add core attributes for JSX

--- tests/tsx.tsx
+++ tests/tsx.tsx
@@ -1,10 +1,17 @@
 export default function test(test: any) {
     return (
         <button
+            hx-boost="true"
             hx-get={test}
-            hx-swap=""
-            hx-target=""
-            hx-trigger=""
+            hx-on:click=""
+            hx-post={test}
+            hx-push-url="true"
+            hx-select="true"
+            hx-select-oob="true"
+            hx-swap="true"
+            hx-target="true"
+            hx-trigger="true"
+            hx-vals='{"myVal": "My Value"}'
         >
         </button>
     )
